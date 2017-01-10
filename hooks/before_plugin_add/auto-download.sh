955b49706fabf642e07c0f7fec31e8180cf81ff4
Kunio SAWATANI <fsawa@fathens.org>
2017-01-10 06:06:10 +0000

    auto-download のフックは Fastlane から生成させる
    Android 全般に関わることなのでプロジェクトごとではなく Fastlane で対応します。

--- hooks/before_plugin_add/auto-download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -vp ~/.android
-
-echo "# Set SDK licenses agree"
-mkdir -vp $ANDROID_HOME/licenses
-echo -e '\n8933bad161af4178b1185d1a37fbf41ea5269c55' > $ANDROID_HOME/licenses/android-sdk-license
-
-echo -e '\nandroid.builder.sdkDownload=true' >> platforms/android/gradle.properties
