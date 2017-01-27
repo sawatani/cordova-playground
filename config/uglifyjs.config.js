affb96ea0acfa0414640c32e3f620f49cf7cefa2
Kunio SAWATANI <fsawa@fathens.org>
2017-01-27 02:16:51 +0000

    バンドルは rollup で行い uglify はダミーにする

---
+++ config/uglifyjs.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  sourceFile: "../../config/empty.js",
+  destFileName: "../../config/.empty.min.js",
+  inSourceMap: "../../config/.empty.min.js",
+  outSourceMap: "../../config/.empty.js.mp",
+  mangle: false,
+  compress: false,
+  comments: false
+};
